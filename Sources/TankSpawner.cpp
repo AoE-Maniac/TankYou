0f238f9dc7b8a691579ba97ef987fea24c3d0483
Max Maag <max-maag@web.de>
2016-09-25 14:02:40 +0200

    Stubs of tank system and spawner.

---
+++ Sources/TankSpawner.cpp
@@ -0,0 +1,7 @@
+#include "TankSpawner.h"
+
+TankSpawner::TankSpawner(vec3 spawn1, vec3 spawn2, float spawnRate) {
+	this->spawn1 = spawn1;
+	this->spawn2 = spawn2;
+	this->spawnRate = spawnRate;
+}
---
+++ Sources/TankSystem.cpp
@@ -0,0 +1,10 @@
+#include "TankSystem.h"
+
+TankSystem::TankSystem(int initialTankCount) {
+	// TODO: init tanks
+	tanks.reserve(initialTankCount);
+}
+
+void TankSystem::update(float dt) {
+
+}
