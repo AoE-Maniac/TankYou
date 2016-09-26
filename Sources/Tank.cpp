dc996c0b19e77cf6354c81c3be462589a4638894
Polona Caserman <polona.caserman@gmail.com>
2016-09-26 18:56:13 +0200

    Tanks wandering

--- Sources/Tank.cpp
+++ Sources/Tank.cpp
@@ -7,7 +7,7 @@ const int MAX_HP = 10;
 Tank::Tank(int frac) : PhysicsObject(COLLIDING_OBJECT::TANK, 10, true, true, true) {
 	Collider.radius = 6.f;
 	turretAngle = 0.f;
-    currentState = Wait;
+    currentState = Wandering;
     steer = new Steering;
     toPosition = vec3(25, yPosition, 15);
 	maxVelocity = 0.5f;
